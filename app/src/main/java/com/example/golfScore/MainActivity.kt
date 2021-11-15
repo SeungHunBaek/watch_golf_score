aa98a38ccb23d40027c15fe4242a38f36add42e0
baek <xemeal@gmail.com>
2021-11-15 23:06:29 +0900

    layout

---
+++ app/src/main/java/com/example/golfScore/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.golfScore
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.golfScore.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
