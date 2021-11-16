a1264590253767ec37c683ddc9cb6e5b2a6ffaaf
baek <xemeal@gmail.com>
2021-11-16 20:22:44 +0900

    비지니스로직추가

--- app/src/main/java/com/example/golfScore/MainActivity.kt
+++ app/src/main/java/com/example/golfScore/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.golfScore
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import com.example.golfScore.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
@@ -14,5 +16,23 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val minus : Button = findViewById(R.id.minus);
+        val plus : Button = findViewById(R.id.plus);
+        val reset : Button = findViewById(R.id.reset);
+        val resultView : TextView = findViewById(R.id.result);
+        // 마이너스 버튼
+        minus.setOnClickListener{
+            val currentScore : Int = Integer.parseInt(resultView.text.toString());
+            resultView.text = (currentScore - 1).toString()
+        }
+        // 플러스 버튼
+        plus.setOnClickListener{
+            val currentScore : Int = Integer.parseInt(resultView.text.toString());
+            resultView.text = (currentScore + 1).toString();
+        }
+        // 리셋 버튼
+        reset.setOnClickListener{
+            resultView.text = 0.toString();
+        }
     }
 }
