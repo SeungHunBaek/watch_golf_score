0e308dad54f84ea2a0d73b47a37afe8477614379
baek <xemeal@gmail.com>
2021-11-17 22:38:32 +0900

    버튼 이벤트에 이미지취득기능 추가

--- app/src/main/java/com/example/golfScore/MainActivity.kt
+++ app/src/main/java/com/example/golfScore/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.golfScore
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.golfScore.databinding.ActivityMainBinding
 
@@ -20,19 +21,32 @@ class MainActivity : Activity() {
         val plus : Button = findViewById(R.id.plus);
         val reset : Button = findViewById(R.id.reset);
         val resultView : TextView = findViewById(R.id.result);
+        val background : LinearLayout = findViewById(R.id.background)
+
         // 마이너스 버튼
         minus.setOnClickListener{
-            val currentScore : Int = Integer.parseInt(resultView.text.toString());
-            resultView.text = (currentScore - 1).toString()
+            val currentScore : Int = Integer.parseInt(resultView.text.toString()) - 1;
+            resultView.text = currentScore.toString();
+            background.setBackgroundResource(getImage(currentScore));
         }
         // 플러스 버튼
         plus.setOnClickListener{
-            val currentScore : Int = Integer.parseInt(resultView.text.toString());
-            resultView.text = (currentScore + 1).toString();
+            val currentScore : Int = Integer.parseInt(resultView.text.toString()) + 1;
+            resultView.text = currentScore.toString();
+            background.setBackgroundResource(getImage(currentScore));
         }
         // 리셋 버튼
         reset.setOnClickListener{
             resultView.text = 0.toString();
+            background.setBackgroundResource(getImage(0));
         }
+    }
+
+    private fun getImage(currentScore: Int): Int {
+        if(currentScore ==  0){
+            return R.drawable.cookie
+        }
+
+        return R.drawable.cookie2
     }
 }
