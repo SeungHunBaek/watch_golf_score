5b157023c581233df1b2936c3a781b498ceb9a8d
baek <xemeal@gmail.com>
2021-11-20 13:21:51 +0900

    이미지추가 및 스코어폰트 수정

--- app/src/main/java/com/example/golfScore/MainActivity.kt
+++ app/src/main/java/com/example/golfScore/MainActivity.kt
@@ -1,8 +1,10 @@
+
 package com.example.golfScore
 
 import android.app.Activity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.example.golfScore.databinding.ActivityMainBinding
 
@@ -20,19 +22,62 @@ class MainActivity : Activity() {
         val plus : Button = findViewById(R.id.plus);
         val reset : Button = findViewById(R.id.reset);
         val resultView : TextView = findViewById(R.id.result);
+        val background : LinearLayout = findViewById(R.id.background)
+
         // 마이너스 버튼
         minus.setOnClickListener{
-            val currentScore : Int = Integer.parseInt(resultView.text.toString());
-            resultView.text = (currentScore - 1).toString()
+            val currentScore : Int = Integer.parseInt(resultView.text.toString()) - 1;
+            resultView.text = currentScore.toString();
+            background.setBackgroundResource(getImage(currentScore));
         }
         // 플러스 버튼
         plus.setOnClickListener{
-            val currentScore : Int = Integer.parseInt(resultView.text.toString());
-            resultView.text = (currentScore + 1).toString();
+            val currentScore : Int = Integer.parseInt(resultView.text.toString()) + 1;
+            resultView.text = currentScore.toString();
+            background.setBackgroundResource(getImage(currentScore));
         }
         // 리셋 버튼
         reset.setOnClickListener{
             resultView.text = 0.toString();
+            background.setBackgroundResource(getImage(0));
+        }
+    }
+
+    private fun getImage(currentScore: Int): Int {
+        when (currentScore) {
+            0 -> {
+                return R.drawable.cnt0
+            }
+            1 -> {
+                return R.drawable.cnt1
+            }
+            2 -> {
+                return R.drawable.cnt2
+            }
+            3 -> {
+                return R.drawable.cnt3
+            }
+            4 -> {
+                return R.drawable.cnt4
+            }
+            5 -> {
+                return R.drawable.cnt5
+            }
+            6 -> {
+                return R.drawable.cnt6
+            }
+            7 -> {
+                return R.drawable.cnt7
+            }
+            8 -> {
+                return R.drawable.cnt8
+            }
+            9 -> {
+                return R.drawable.cnt9
+            }
+            else -> {
+                return R.drawable.cnt10
+            }
         }
     }
 }
